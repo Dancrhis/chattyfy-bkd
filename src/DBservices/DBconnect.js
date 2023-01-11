aa0000a43d175fb84f79db880ef067d691c76ab6
Dancrhis <cristophermartinez1989@gmail.com>
2023-01-11 17:50:41 -0400

    dsdsds

--- src/DBservices/DBconnect.js
+++ src/DBservices/DBconnect.js
@@ -1,11 +1,14 @@
 import { MongoClient } from "mongodb";
-const url = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(url);
+
+import mongoose from "mongoose";
+const URL = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(URL);
 const DBName = "chattify";
 export const db = client.db(DBName);
 export const USERS_COLLECTION="users";
 
 export async function DBconnect() {
+  mongoose.connect(`${URL}/${DBName}`).then(()=>console.log("conectado a la base de datos"))
   await client.connect();
   console.log("conectado a la base de datos");
 }
---
+++ src/schema/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+    },
+    lastname:{
+        type:String,
+        require:true,
+    },
+    username:{
+        type:String,
+        require:true,
+        
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+    
+})
+
+export const userModel=mongoose.model("users",userSchema,"users")
